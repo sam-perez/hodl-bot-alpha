eacb188b72180d21da826addad838256589199a4
Samuel Perez <samuel.e.perez@gmail.com>
2019-06-28 18:07:48 -0700

    how much are we gonna buy

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 'use strict';
-const axios = require('axios');
+//const axios = require('axios');
+//await axios.post(process.env.SLACK_POST_URL, { text: `HODL BOT RUNNING @ ${currentTime}` });
+const airtableLib = require('./lib/airtable');
+const strategyHelperLib = require('./lib/strategy_helper');
+const R = require('ramda');
 
 module.exports.main = async (event, context, callback) => {
   try {
-    const currentTime = new Date().toTimeString();
+    const { allBuyConfigs } = await airtableLib.getAllBuyConfigs();
 
-    await axios.post(process.env.SLACK_POST_URL, { text: `HODL BOT RUNNING @ ${currentTime}` });
+    const activeConfigs = R.filter(
+      buyConfig => strategyHelperLib.isConfigActive({ buyConfig }),
+      allBuyConfigs
+    );
+
+    console.log({ activeConfigs });
+
+    for (const buyConfig of activeConfigs) {
+      console.log('Working on the following config', { buyConfig });
+
+      const { amountToBuyInDollars } = strategyHelperLib.getBuyAmountInDollarsForConfig({
+        buyConfig
+      });
+
+      console.log({ amountToBuyInDollars });
+    }
   } catch (error) {
     console.log('Error during lambda execution', { error });
     callback(error);
